0d3cccfd79c728bae95a584c47d1934a4f3aab92
aliaksandr <plcgi1@gmail.com>
2023-09-07 12:55:37 +0300

    version and added dist

---
+++ dist/enums.d.ts
@@ -0,0 +1,134 @@
+export declare const BCRYPT_SALT: number;
+export declare enum SYSTEM_USER_ROLES_ENUM {
+    user = "user",
+    admin = "admin",
+    root = "root"
+}
+export declare const SYSTEM_USER_ROLES_AS_OBJECT: {
+    [key: string]: string;
+};
+export declare enum USER_STATUSES_ENUM {
+    waiting = "waiting",
+    ready = "ready",
+    blocked = "blocked",
+    deleted = "deleted"
+}
+export declare const USER_STATUSES_AS_OBJECT: {
+    [key: string]: string;
+};
+export declare enum ORG_TYPE_ENUM {
+    org = "org",
+    location = "location"
+}
+export declare const ORG_TYPE_AS_OBJECT: {
+    [key: string]: string;
+};
+export declare enum TOKEN_TYPE_ENUM {
+    confirm = "confirm",
+    auth = "auth",
+    refresh = "refresh",
+    resetPassword = "resetPassword",
+    invite = "invite"
+}
+export declare const TOKEN_TYPES_AS_OBJECT: {
+    [key: string]: string;
+};
+export declare enum ANIMAL_GENDER_ENUM {
+    cow = "cow",
+    bull = "bull"
+}
+export declare const ANIMAL_GENDER_AS_OBJECT: {
+    [key: string]: string;
+};
+export declare enum EVENT_TYPES_ENUM {
+    otel = "otel",
+    recheck = "recheck",
+    ohota = "ohota",
+    osemenenie = "osemenenie",
+    stelnaya = "stelnaya",
+    yalovaya = "yalovaya",
+    stelneotpos = "stelneotpos",
+    perevod = "perevod",
+    kbiku = "kbiku",
+    suhostoi = "suhostoi",
+    abort = "abort",
+    neosem = "neosem",
+    prodazha = "prodazha",
+    pala = "pala",
+    zamenaNomera = "zamenaNomera",
+    obrKopit = "obrKopit",
+    pozSuhostoi = "pozSuhostoi",
+    ves = "ves",
+    rost = "rost",
+    upitannost = "upitannost",
+    zaboi = "zaboi",
+    sinchronizatsia = "sinchronizatsia"
+}
+export declare const EVENT_TYPES_AS_OBJECT: {
+    [key: string]: string;
+};
+export declare enum SEED_TYPE_ENUM {
+    traditional = "traditional",
+    sexytelka = "sexytelka",
+    sexybull = "sexybull",
+    meat = "meat",
+    meatSexyTelka = "meatSexyTelka",
+    meatSexyBull = "meatSexyBull"
+}
+export declare const SEED_TYPE_AS_OBJECT: {
+    [key: string]: string;
+};
+export declare enum SEED_STATUS_ENUM {
+    active = "active",
+    notActive = "notActive"
+}
+export declare const SEED_STATUS_AS_OBJECT: {
+    [key: string]: string;
+};
+export declare enum SEED_CODES_ENUM {
+    DOUBLE_OVSYNC = "doubleOvsync",
+    PRESYNC = "dresync",
+    RESYNC = "resync",
+    OVSYNC = "ovsync",
+    HEAT = "heat",
+    ACTIVITY_SENSOR = "activitySensor",
+    PRID_DELTA = "PRIDDelta",
+    G6G = "G6G"
+}
+export declare const SEED_CODES_AS_OBJECT: {
+    [key: string]: string;
+};
+export declare enum ANIMAL_STATUS_ENUM {
+    heifer = "heifer",
+    bred = "bred",
+    pregnant = "pregnant",
+    open = "open",
+    fresh = "fresh",
+    dry = "dry",
+    dnb = "dnb",
+    sold = "sold",
+    died = "died"
+}
+export declare const ANIMAL_STATUS_AS_OBJECT: {
+    [key: string]: string;
+};
+export declare enum EVENT_ERROR_REASON_ENUM {
+    statusNotAllowed = "animal-status-not-allowed",
+    statusNotAlowed_pregnant = "animal-status-not-allowed-pregnant",
+    statusNotAlowed_dry = "animal-status-not-allowed-dry",
+    statusNotAlowed_dnb = "animal-status-not-allowed-dnb",
+    statusNotAlowed_sold = "animal-status-not-allowed-sold",
+    statusNotAlowed_died = "animal-status-not-allowed-",
+    notACow = "animal-not-a-cow",
+    tooYoung = "animal-too-yang"
+}
+export declare const EVENT_ERROR_REASON_AS_OBJECT: {
+    [key: string]: string;
+};
+export declare enum COMMON_SIMPLE_STATUS_ENUM {
+    failed = "failed",
+    success = "success"
+}
+export declare const COMMON_SIMPLE_STATUS_AS_OBJECT: {
+    [key: string]: string;
+};
---
+++ dist/types.d.ts
@@ -0,0 +1,8 @@
+export interface EnumItem {
+    readonly id: string;
+    readonly label: string;
+    readonly color?: string;
+}
+export declare type EnumArrayAsObject = {
+    [key: string]: string;
+};
