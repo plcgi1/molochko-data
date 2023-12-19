d52f97d43785a5933345f40f1f47c3fc1a6a729f
aliaksandr <plcgi1@gmail.com>
2023-12-19 11:55:06 +0300

    role permissions

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import * as dataEnums from "./enums";
 import { BREED, BREED_AS_OBJECT } from "./breed";
 import * as typeDefinitions from "./types";
+import * as _rolePermissions from './role-permissions'
 
 export const enums = { ...dataEnums, BREED, BREED_AS_OBJECT };
+export const rolePermissions = _rolePermissions
 export const types = typeDefinitions;
