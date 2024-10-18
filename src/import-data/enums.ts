e45bf6d0842f62045215cbe727f7ccd77c99cca2
alex <plcgi1@gmail.com>
2024-10-18 12:36:02 +0300

    import-enums

---
+++ src/import-data/enums.ts
@@ -0,0 +1,7 @@
+import { enumToObject } from '../helpers';
+
+export enum IMPDATA_SOURCES_ENUM {
+    dc = 'dc',
+}
+
+export const IMPDATA_SOURCES_ENUM_AS_OBJECT = enumToObject(IMPDATA_SOURCES_ENUM);
