06101892a940d70d5eb5ba25a2a63e3a82a74f26
alex <plcgi1@gmail.com>
2024-10-18 12:40:40 +0300

    v

--- src/import-data/enums.ts
+++ src/import-data/enums.ts
@@ -5,3 +5,11 @@ export enum IMPDATA_SOURCES_ENUM {
 }
 
 export const IMPDATA_SOURCES_ENUM_AS_OBJECT = enumToObject(IMPDATA_SOURCES_ENUM);
+
+export enum IMPDATA_STATUS_ENUM {
+  working = 'working',
+  ready = 'ready',
+  error = 'error'
+}
+
+export const IMPDATA_STATUS_ENUM_AS_OBJECT = enumToObject(IMPDATA_STATUS_ENUM);
--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,11 @@ import * as typeDefinitions from './types';
 import * as _rolePermissions from './role-permissions';
 import * as _animalForEventFilters from './event';
 import * as _clEnums from './custom-list';
+import * as _importEnums from './import-data'
 
 export const enums = { ...dataEnums, BREED, BREED_AS_OBJECT };
 export const rolePermissions = _rolePermissions;
 export const types = typeDefinitions;
 export const customListEnums = _clEnums.enums;
-
+export const importEnums = _importEnums
 export const animalForEventFilters = _animalForEventFilters;
