43d29c22820114652f7e548a7661340211567d13
aliaksandr <plcgi1@gmail.com>
2024-10-22 11:56:52 +0300

    import enums

--- src/import-data/enums.ts
+++ src/import-data/enums.ts
@@ -1,11 +1,9 @@
-import { enumToObject } from '../helpers';
-
 export enum IMPDATA_SOURCES_ENUM {
   diaryComp = 'diaryComp',
 }
 
 export enum IMPDATA_STATUS_ENUM {
-	waiting = 'waiting',
+  waiting = 'waiting',
   working = 'working',
   ready = 'ready',
   error = 'error',
