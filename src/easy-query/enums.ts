c3a83c15b3083b1c934f4f1a4bd0e3d0c73e1b41
alex <plcgi1@gmail.com>
2025-08-21 14:21:54 +0300

    eq - forbidden-words

--- src/easy-query/enums.ts
+++ src/easy-query/enums.ts
@@ -7,3 +7,5 @@ export enum EQ_STATUS_ENUM {
   working = 'working',
   published = 'published',
 }
+
+export const EQ_FORBIDDEN_WORDS =  ['*','insert', 'update', 'delete', 'drop', 'create', 'alter', 'change']
